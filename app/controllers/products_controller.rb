98ba0c23655d6bc6e201ecc880be769be8e3c549
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-05-21 14:16:38 +0530

    error rise at delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,47 +1,51 @@
 class ProductsController < ApplicationController
-    layout 'admin/adminDashboard'
-    def index
-      @products=Product.all
-    end
-  
-    def new
-      @product=Product.new
-    end
-  
-    def create
-      #render plain: params[:product].inspect
-      @product=Product.new(product_params)
-      if  @product.save
-      flash[:notice] = "record created!!!!"
-      redirect_to admindashboard_path
-      else
-        render :new
-      end
-    end
-  
-    def edit
-      @product=Product.find(params[:id])
-    end
-  
-    def update
-      @product=Product.find(params[:id])
-      if(@product.update(product_params))
-        flash[:notice] = "record updated!!!!"
-        redirect_to admindashboard_path
-      else
-        render :edit
-      end
-  
-    end
-  
-    def destroy
-      @product=Product.find(params[:id])
-      @product.destroy
-      redirect_to admindashboard_path
-    end
-    private
-    def product_params
-        params.require(:product).permit(:product_name)
+  layout 'admin/adminDashboard'
+  def index
+    @products=Product.all
+  end
+
+  def new
+    @product=Product.new
+  end
+
+  def create
+    #render plain: params[:product].inspect
+    @product=Product.new(product_params)
+    if  @product.save
+    flash[:notice] = "record created!!!!"
+    redirect_to admindashboard_path
+    else
+      render :new
     end
   end
-  
+
+  def edit
+    @product=Product.find(params[:id])
+  end
+
+  def update
+    @product=Product.find(params[:id])
+    if(@product.update(product_params))
+      flash[:notice] = "record updated!!!!"
+      redirect_to admindashboard_path
+    else
+      render :edit
+    end
+
+  end
+
+
+  def show
+    @product=Product.find(params[:id])
+  end
+
+  def destroy
+    @product=Product.find(params[:id])
+    @product.destroy
+    redirect_to admindashboard_path
+  end
+  private
+  def product_params
+      params.require(:product).permit(:product_name)
+  end
+end
