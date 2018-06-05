63dfb147bb50449c84c340b51fb5699f36aff06a
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-06-05 18:53:19 +0530

    initial card click navigate to login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+	layout 'home_layout'
     protected
-   
-
     def after_sign_up_path_for(resource)
         puts current_user.role
     end
