4d89d61acd54d7ad4b7853d7290c1a509ec381ed
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-06-06 15:31:08 +0530

    removed cart related migrations

--- db/migrate/20180525100452_add_product_category_reference_to_cart.rb
+++
@@ -1,6 +0,0 @@
-class AddProductCategoryReferenceToCart < ActiveRecord::Migration
-  def change
-    add_reference :carts, :product_category, index: true
-    add_foreign_key :carts, :product_categories
-  end
-end
